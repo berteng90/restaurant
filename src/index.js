a766ed239b323a31add463830e3c76490a1e4bea
Your Name <berteng90@gmail.com>
2022-12-27 21:42:15 +0800

    Update Files

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
+require('./style.css');
+
 function component(){
     const header=document.createElement('header');
+    const div=document.createElement('div');
+    div.classList.add('nav');
     const home=document.createElement('h1');
+    home.textContent='HOME';
     const menu=document.createElement('h1');
+    menu.textContent='MENU';
     const contact=document.createElement('h1');
-    header.appendChild(home);
-    header.appendChild(menu);
-    header.appendChild(menu);
-
+    contact.textContent='CONTACT US';
+    div.appendChild(home);
+    div.appendChild(menu);
+    div.appendChild(contact);
     const content=document.createElement('div');
-   
-    console.log("test");
-    return [header, content];
+    content.classList.add('wrapper')
+    document.body.appendChild(header);
+    document.body.appendChild(content);
+    header.appendChild(div);
+    const par1=document.createElement('p');
+    const img1=document.createElement('img');
+    par1.textContent="Best Fried Chicken in Town!";
+    content.appendChild(par1);
+    content.appendChild(img1);
+
 }
-document.body.appendChild(component());
+
+component();
